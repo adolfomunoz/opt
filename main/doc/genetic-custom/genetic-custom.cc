cbfc760d7c673a6c8d83c525c4db1152821464ef
Adolfo <adolfo@unizar.es>
2018-05-11 22:32:28 +0200

    Detected bug in gcc7.3.0 with concepts involving lambda functions. Workaround by explicitly indicating lambda functions return type.

--- main/doc/genetic-custom/genetic-custom.cc
+++ main/doc/genetic-custom/genetic-custom.cc
@@ -13,14 +13,14 @@ int main(int argc, char** argv) {
 		// Genetic method with 100 iterations, 10 population, 10 mutations and 5 crossovers per iteration
 		opt::genetic(100,10,10,5),
 		// Mutation strategy: linear probability from distance 0 to 10
-		[] (double r, auto& random) {	
+		[] (double r, auto& random) -> double {	
 			std::array<double,3> values{r-10.0, r, r+10.0};
 			std::array<double,3> probs{0.0, 1.0, 0.0};
 			std::piecewise_linear_distribution<double> d(values.begin(), values.end(), probs.begin());
 			return d(random);		
 		},
 		// Crossover strategy: linear interpolation
-		[] (double r1, double r2, auto& random) {
+		[] (double r1, double r2, auto& random) -> double {
 			std::uniform_real_distribution<double> d(r1,r2);
 			return d(random);
 		}
